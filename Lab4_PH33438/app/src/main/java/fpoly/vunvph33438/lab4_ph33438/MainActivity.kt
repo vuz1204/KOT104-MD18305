914f277d2abe5085c8b14de47c3f36a526c41f83
Nguyễn Văn Vũ <vanvu101204@gmail.com>
2024-05-22 14:12:22 +0700

    Nop lab 4

---
+++ Lab4_PH33438/app/src/main/java/fpoly/vunvph33438/lab4_ph33438/MainActivity.kt
@@ -0,0 +1,227 @@
+package fpoly.vunvph33438.lab4_ph33438
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            // Task 1
+//            LoginScreen()
+
+            // Task 2
+//            val images = listOf(R.drawable.image1, R.drawable.image2, R.drawable.image3)
+//            Column {
+//                Icon(
+//                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+//                    contentDescription = "Logo"
+//                )
+//                HorizontalImageList(images)
+//                VerticalImageList(images)
+//            }
+
+            // Task 3
+            Scaffold(floatingActionButton = {
+                FloatingActionButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Add, contentDescription = "Add")
+                }
+            }) { innerPadding ->
+                NoteApp(paddingValues = innerPadding)
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginScreen() {
+    val context = LocalContext.current
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        // Logo
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Logo"
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Username TextField
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Password TextField
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Login Button
+        Button(
+            onClick = {
+                if (username.isNotBlank() || password.isNotBlank()) {
+                    Toast.makeText(
+                        context, "Login successful", Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        context, "Please enter username and password", Toast.LENGTH_LONG
+                    ).show()
+                }
+            }, colors = ButtonDefaults.buttonColors(
+                containerColor = Color.DarkGray, contentColor = Color.White
+            )
+        ) {
+            Text(text = "Login")
+        }
+    }
+}
+
+@Composable
+fun HorizontalImageList(imageList: List<Int>) {
+    val scrollState = rememberScrollState()
+
+    Row(
+        modifier = Modifier
+            .horizontalScroll(scrollState)
+            .padding(16.dp)
+    ) {
+        imageList.forEachIndexed { index, image ->
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = "Image Description",
+                contentScale = ContentScale.FillHeight,
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .padding(
+                        start = if (index == 0) 0.dp else 8.dp, end = 8.dp
+                    )
+            )
+        }
+    }
+}
+
+@Composable
+fun VerticalImageList(imageList: List<Int>) {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .padding(16.dp)
+    ) {
+        imageList.forEachIndexed { index, image ->
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = "Image Description",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .padding(top = if (index == 0) 0.dp else 8.dp, bottom = 8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun NoteApp(paddingValues: PaddingValues) {
+    val notes = listOf("Note 1", "Note 2", "Note 3", "Note 4", "Note 5")
+
+    Column(
+        modifier = Modifier
+            .padding(paddingValues)
+            .padding(8.dp)
+    ) {
+        notes.forEach { note ->
+            NoteCard(noteText = note)
+        }
+    }
+}
+
+@Composable
+fun NoteCard(noteText: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(color = Color.LightGray, shape = MaterialTheme.shapes.medium)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = noteText,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(16.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Icon(
+                imageVector = Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Expand Note",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically)
+            )
+        }
+    }
+}
